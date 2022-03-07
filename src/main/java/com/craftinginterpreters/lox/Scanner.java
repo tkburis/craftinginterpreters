a0c6243ab834d27ba3bdf73597fdddab8d600a12
tkburis <tkburis@gmail.com>
2022-03-07 21:16:59 +0000

    Initial commit

---
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,133 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    
+    private int start = 0;  // Beginning of lexeme.
+    private int current = 0;  // Current position.
+    private int line = 1;
+    
+    Scanner(String source) {
+	this.source = source;
+    }
+    
+    List<Token> scanTokens() {
+	while (!isAtEnd()) {
+	    start = current;
+	    scanToken();
+	}
+	
+	tokens.add(new Token(EOF, "", null, line));
+	return tokens;
+    }
+    
+    private void scanToken() {
+	char c = advance();
+	switch (c) {
+	    case '(': addToken(LEFT_PAREN); break;
+	    case ')': addToken(RIGHT_PAREN); break;
+	    case '{': addToken(LEFT_BRACE); break;
+	    case '}': addToken(RIGHT_BRACE); break;
+	    case ',': addToken(COMMA); break;
+	    case '.': addToken(DOT); break;
+	    case '-': addToken(MINUS); break;
+	    case '+': addToken(PLUS); break;
+	    case ';': addToken(SEMICOLON); break;
+	    case '*': addToken(STAR); break;
+	    
+	    case '!':
+		addToken(match('=') ? BANG_EQUAL : BANG);
+		break;
+	    case '=':
+		addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+		break;
+	    case '<':
+		addToken(match('=') ? LESS_EQUAL : LESS);
+		break;
+	    case '>':
+		addToken(match('=') ? GREATER_EQUAL : GREATER);
+		break;
+		
+	    case '/':
+		if (match('/')) {
+		    // A comment goes until the end of the line, ignore everything.
+		    while (peek() != '\n' && !isAtEnd()) advance();  // Once we know the next one isn't \n, we can move on.
+		} else {
+		    addToken(SLASH);
+		}
+		break;
+	    
+	    case ' ':
+	    case '\r':
+	    case '\t':
+		// Ignore whitespace.
+		break;
+		
+	    case '\n':
+		line++;
+		break;
+		
+	    case '"': string(); break;
+		
+	    default:
+		Lox.error(line, "Unexpected character");
+		break;
+	}
+    }
+    
+    private void string() {
+	while (peek() != '"' && !isAtEnd()) {
+	    if (peek() == '\n') line++;
+	    advance();
+	}
+	
+	if (isAtEnd()) {
+	    Lox.error(line, "Unterminated string.");
+	    return;
+	}
+	
+	advance();  // The closing ".
+	
+	// Trim the surrounding quotes.
+	String value = source.substring(start + 1, current - 1);
+	addToken(STRING, value);
+    }
+    
+    private boolean match(char expected) {
+	if (isAtEnd()) return false;
+	if (source.charAt(current) != expected) return false;
+	
+	current++;
+	return true;
+    }
+    
+    private char peek() {  // Similar to advance(), but we don't increment current.
+	if (isAtEnd()) return '\0';
+	return source.charAt(current);
+    }
+    
+    private boolean isAtEnd() {
+	return current >= source.length();
+    }
+    
+    private char advance() {  // Returns current character and moves onto next.
+	return source.charAt(current++);
+    }
+    
+    private void addToken(TokenType type) {
+	addToken(type, null);
+    }
+    
+    private void addToken(TokenType type, Object literal) {
+	String text = source.substring(start, current);
+	tokens.add(new Token(type, text, literal, line));
+    }
+}
