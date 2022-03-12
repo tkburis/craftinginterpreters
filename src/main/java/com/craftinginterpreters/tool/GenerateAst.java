cbed32de8baac444d2fe3c7c73c6f4cd02c44605
tkburis <tkburis@gmail.com>
2022-03-12 15:07:50 +0000

    Completed chapter 5

--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -34,15 +34,33 @@ public class GenerateAst {
 	writer.println();
 	writer.println("abstract class " + baseName + " {");
 	
+	defineVisitor(writer, baseName, types);
+	writer.println();
+	
 	// The AST classes.
 	for (String type : types) {
 	    String className = type.split(":")[0].trim();
 	    String fields = type.split(":")[1].trim();
 	    defineType(writer, baseName, className, fields);
+	    writer.println();
 	}
+	
+	// The base accept() method.
+	writer.println("    abstract <R> R accept(Visitor<R> visitor);");
 	
 	writer.println("}");
 	writer.close();
+    }
+    
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+	writer.println("    interface Visitor<R> {");
+	
+	for (String type : types) {
+	    String typeName = type.split(":")[0].trim();
+	    writer.println("	R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+	}
+	
+	writer.println("    }");
     }
     
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -55,9 +73,16 @@ public class GenerateAst {
 	String[] fields = fieldList.split(", ");
 	for (String field : fields) {
 	    String name = field.split(" ")[1];
-	    writer.println("		this." + name + " = " + name + ";");
+	    writer.println("	    this." + name + " = " + name + ";");
 	}
 	
+	writer.println("	}");
+	
+	// Visitor pattern.
+	writer.println();
+	writer.println("	@Override");
+	writer.println("	<R> R accept(Visitor<R> visitor) {");
+	writer.println("	    return visitor.visit" + className + baseName + "(this);");
 	writer.println("	}");
 	
 	// Fields.
